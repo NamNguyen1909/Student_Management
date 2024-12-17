3998ef7a48c5fed1dcef49e10c7272e2a932feba
NamNguyen1909 <namnguyen19092004@gmail.com>
2024-12-17 19:37:51 +0700

    index, login, base, model

--- Student_Management/app/index.py
+++ Student_Management/app/index.py
@@ -1,2 +1,27 @@
-if __name__ == '__main__':
-    print('Hello World!')
+
+from flask import render_template
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/login", methods=['get', 'post'])
+def login_process():
+    # if request.method.__eq__('POST'):
+    #     username = request.form.get('username')
+    #     password = request.form.get('password')
+    #
+    #     user = dao.auth_user(username=username, password=password)
+    #     if user:
+    #         login_user(user)
+    #
+    #         next=request.args.get('next')
+    #         return redirect('/' if next is None else next)
+
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
